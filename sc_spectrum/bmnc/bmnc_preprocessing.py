3033c2f8439eb21d2ba6399504eca3a62e34aaf6
Jacob Leistico <jake@Jacobs-MacBook-Pro.local>
2022-01-03 19:06:48 -0600

    Bug fixes in bmnc_preprocessing.py

--- sc_spectrum/bmnc/bmnc_preprocessing.py
+++ sc_spectrum/bmnc/bmnc_preprocessing.py
@@ -8,8 +8,7 @@ Created on Wed Nov 17 15:33:12 2021
 
 import pandas as pd
 
-from model.preprocessing import rna_pca, clr_transform 
-
+from sc_spectrum.preprocessing import rna_pca, clr_transform 
 
 def preprocess_rna(
     rna_file: str,
@@ -17,9 +16,22 @@ def preprocess_rna(
     random_state: int,
 ) -> pd.DataFrame:
     """
+    Process the RNA count matrix for the CBMC dataset.
     
-    
-    
+    Arguments
+        rna_file : The file containing the RNA count matrix
+        n_pc : How many pricipal components to compute. If None all principal
+            components will be computed. If an integer is specieid this number
+            of principal components will be return. If a float is given the
+            number of principal components needed to explain this fraction of
+            the variance will be returned
+        random_state : The random state to use for the 'randomized' solver.
+            This is only used when an integer value for n_pc is given
+            
+    Returns
+        cbmc_rna_pca : A pandas DataFrame with the RNA principal components.
+            Rows correspond to cells and columns to principal components
+            
     """
     
     rna_count = pd.read_csv(rna_file, header = 0, index_col = 0)
@@ -33,7 +45,15 @@ def preprocess_adt(
     adt_file: str,
 ) -> pd.DataFrame:
     """
+    Perform centered log ratio (CLR) transform of the ADT count data.
     
+    Arguments:
+        adt_file : The file containing the ADT count matrix
+            
+    Returns:
+        cbmc_adt_human_clr : A pandas DataFrame with the CLR transformed ADT 
+            profiles. Rows correspond to cells and columns to ADT antibodies.
+            
     """
     
     bm_adt_count = pd.read_csv(adt_file, header = 0, index_col = 0)
